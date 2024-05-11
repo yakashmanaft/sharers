8d69bebd47b44370d0bfc272bd633f206032072a
yakashmanaft <ya.kashmanaft@yandex.ru>
2024-05-11 20:29:21 +0500

    добавили динамическое отображение предметов типа stuff на странице конкретного предмеета :id

--- pages/warehouse/[id].vue
+++ pages/warehouse/[id].vue
@@ -35,12 +35,37 @@ const route = useRoute();
 //
 const items = ref(null);
 const item = ref(null);
+const itemLocations = ref(null);
 
 onMounted(async () => {
   //
   items.value = await getItems();
   item.value = items.value.find((item: any) => item.id == route.params.id);
+
+  itemLocations.value = items.value.filter((element) => {
+    if(element.type === 'stuff' && element.title === item.value.title) {
+      return element
+    }
+  })
 });
+console.log(route.path.split('/', 3))
+// if(route.path.split('/', 3)[1] === 'warehouse' && route.path.split('/', 3)[2] === $route.params.id)
+
+const locationLinkColorized = (location: string, id: number) => {
+
+  if (location) {
+
+    if(+route.params.id === id) {
+      return `link_${location} link_current-route`;
+    } else {
+      return `link_${location}`
+    }
+  }
+};
+
+const translateLocation = (id: any, location: string) => {
+  return `${location} ${id}`
+}
 
 //
 /**
@@ -60,24 +85,28 @@ async function getItems() {
 
         <h1>{{ item.title }}</h1>
           <!-- Материалы -->
-          <div v-if="item.type === 'stuff'" style="display: flex; gap: 1rem;">
+          <div v-if="item.type === 'stuff'" style="margin-top: 1rem; display: flex; gap: 1rem;">
             <!-- Показать динамически места, в которых есть данный материал. Стили как в общем списке ТЦ. По клику переходим на ТМЦ по выбранному месту. Выделить текущее местоположение выбранного ТМЦ --> 
-            <div>
-              Всего (36 шт.)
+            <div v-if="itemLocations.length > 1">
+              <span class="link link-location">
+                Всего ()
+              </span>
             </div>
-            <div>
-              Офис на Хасана (12 шт)
-            </div>
-            <div>
-              Монолит G14 THULE (12 шт)
-            </div>
-            <div>
-              Клиника (12 шт)
-            </div>
+            <div v-for="(location, i) in itemLocations" :key="i">
+
+            <router-link :to="`${location.id}`" style="text-decoration: none;">
+              <span
+                class="link link-location"
+                :class="`${locationLinkColorized(location.location, location.id)}`"
+              >
+                {{ translateLocation(location.locationID, location.location) }} ({{ location.qty }} {{ location.measure }})
+              </span>
+            </router-link>
+          </div>
         </div>
       </div>
-
       <!-- из warehouse page - warehouseCategories - перенести в store, чтобы можно было дергать сразу переведенный текст -->
+      <br>
       <h2>{{ item.type }}</h2>
 
       <!-- Инструмент -->
@@ -128,3 +157,44 @@ async function getItems() {
     </div>
   </Container>
 </template>
+
+<style scoped>
+.link-location {
+  padding: 4px 10px;
+  border-radius: 16px;
+}
+.link:hover {
+  cursor: pointer;
+}
+.link_project {
+  color: var(--bs-success);
+  border: 1px solid var(--bs-success-bg-subtle);
+  background-color: var(--bs-success-bg-subtle);
+}
+.link_sklad {
+  color: white;
+  border: none;
+  background-color: var(--bs-primary-bg-subtle);
+}
+.link_office {
+  color: white;
+  border: none;
+  background-color: var(--bs-primary-bg-subtle);
+}
+.link_repair {
+  color: var(--bs-warning);
+  border: 1px solid var(--bs-warning-bg-subtle);
+  background-color: var(--bs-warning-bg-subtle);
+}
+.link_archive {
+  color: var(--bs-dark-bg-subtle);
+  border: none;
+}
+.link_deleted {
+  color: var(--bs-danger-bg-subtle);
+  border: none;
+}
+.link_current-route {
+  color: red;  
+}
+</style>

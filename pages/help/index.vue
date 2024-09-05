9399e5515876ac3b9c3518f2067edb056aae1a33
yakashmanaft <ya.kashmanaft@yandex.ru>
2024-09-06 00:03:31 +0500

    набросали заготовку структуры разных бд для статей в раззделе помощь

--- pages/help/index.vue
+++ pages/help/index.vue
@@ -6,87 +6,33 @@
     <div>
       <h2>База знаний</h2>
 
-      <!-- MODULE: PROJECTS -->
-      <div>
-        <h3>Модуль: Проекты</h3>
-
+      <div class="article_wrapper" v-for="article in articles">
+        <h3>{{ article.title }}</h3>
+        <p>{{  article.text  }}</p>
+        <!-- {{ article }} -->
         <!--  -->
-        <div>
-          <h4>Вкладки</h4>
-          <p>
-            Вкладки "План-график", "Заявки", "Соучастники", "ТМЦ", "Баланс"
-            доступны в зависимости от статуса пользвателя: partner / creator /
-            curator / else
-          </p>
+        <div v-for="element in article_elements.filter(element => element.parent_article_id === article.id)">
+          <h4>{{ element.title }}</h4>
+          <p>{{  element.text  }}</p>
+          <!-- {{ element }} -->
+          <!--  -->
+          <div v-for="part in element_parts.filter(part => part.parent_element_id === element.id)">
+            <h5>{{ part.title }}</h5>
+            <!-- <img :src="part.img_code" alt=""> -->
+             {{ part.img_code }}
+            <ul>
+              <!-- Как сделать сплит через переиспользуемую в дальнейшем функцию? п типу make_text_splt(part.text, '*|*') -->
+              <li v-for="part_paragraph in part.text?.split('*|*')">
+                {{ part_paragraph }}
+              </li>
+            </ul>
 
-          <h5>Условия доступа к вкладкам</h5>
-          <ul>
-            <li>
-              Если пользователь является partner / creator / curator виидит все
-              Вкладки
-            </li>
-            <li>
-              Если пользователь числится в соучастниках по проекту видит:
-              "План-график", "Заявки", "Соучастники", "ТМЦ"
-            </li>
-            <li>Если польтзователь является else - только "Соучастники"</li>
-          </ul>
-
-          <h5>Вкладка План-график</h5>
-          <ul>
-            <li></li>
-          </ul>
-
-          <h5>Вкладка Соучастники</h5>
-          <ul>
-            <li></li>
-          </ul>
+            <!-- {{part}} -->
+          </div>
         </div>
+
       </div>
-      <!-- MODULE: WAREHOUSE -->
-      <div>
-        <h3>Модуль: Склад</h3>
-
-        <!--  -->
-        <div>
-          <h4>Фильтрация предметов</h4>
-          <p>
-            При загрузке страницы
-            <router-link to="/warehouse" class="link">ТМЦ</router-link>
-            пользователю выводится список:
-          </p>
-
-          <h5>Условия показа</h5>
-          <ul>
-            <li>Пользователь является собственником предмета</li>
-            <li>
-              Пользовтатель является ответственным по предмету (в случае, если
-              флаг "Показывать всем в банде" отключен)
-            </li>
-            <li>Предметы банды, если пользователь является участников банды</li>
-            <li>
-              Предметы яльянса, если банда, соучастником которой является
-              пользователь
-            </li>
-            <li>
-              Предметы других пользователей, если они состоят в банде или
-              альянсе, как и сам пользователь
-            </li>
-          </ul>
-
-          <h5>Скрытие показа предмета на складе</h5>
-          <ul>
-            <li>
-              Если пользователем является собственником предмета, он может
-              скрыть предмет от показза другим участникам банды/альянса.
-            </li>
-            <li>
-              Скрытый предмет будет видеть только собствевннику и, если указан,
-              ответственному по данному предмету.
-            </li>
-          </ul>
-        </div>
-      </div>
+      <br>
 
       <!-- MODULE: SHARERS -->
       <div>
@@ -274,6 +220,148 @@ useHead({
   ],
 });
 import { Container } from "@/shared/container";
+
+// ПЕРЕМЕННЫЕ
+// const module_articles = ref([
+//   {
+//     module_name: 'Список проектов',
+//     elements: [
+//       {
+//       }
+//     ]
+//   },
+//   {
+//     module_name: 'Конкретный проект',
+//     elements: [
+//       {
+//         title: 'Вкладки',
+//         paragraph: ''
+//       }
+//     ]
+//   },
+//   {
+//     module_name: 'Склад'
+//   },
+//   {
+//     module_name: 'Соучастники'
+//   }
+// ])
+
+// Парсить несколько бд и мапить новый массив
+// db список разделов
+// id | article_name | article_paragraph
+const articles = ref([
+  {
+    id: 1,
+    name: 'projects-list',
+    title: 'Список проектов',
+    type: 'modules',
+    text: ''
+  },
+  {
+    id: 2,
+    name: 'current-project',
+    title: 'Конкретный проект',
+    type: 'modules',
+    text: ''
+  },
+  {
+    id: 3,
+    name: 'warehouse',
+    title: 'Список тмц',
+    type: 'modules',
+    text: ''
+  },
+  {
+    id: 4,
+    name: 'warehouse-item',
+    title: 'Конкретный тмц',
+    type: 'modules',
+    text: ''
+  },
+  {
+    id: 5,
+    name: 'list-sharers',
+    title: 'Соучасники',
+    type: 'modules',
+    text: ''
+  },
+  {
+    id: 6,
+    name: 'current-sharer',
+    title: 'Конкретный соучастник',
+    type: 'modules',
+    text: ''
+  }
+])
+
+// db элементы разделов
+// id | element_title | element_paragraph
+const article_elements = ref([
+  {
+    id: 1,
+    parent_article_id: 2,
+    title: 'Вкладки',
+    text: 'Доступны в зависимости от статуса пользователя: partner / creator / curator / else'
+  },
+  {
+    id: 2,
+    parent_article_id: 3,
+    title: 'Фильтрация предметов',
+    text: 'При загрузке страницы <router-link to="/warehouse" class="link">ТМЦ</router-link> пользователю выводится список:'
+  }
+])
+
+// db подэлементы элементов
+// text must be splited by sign *|*
+const element_parts = ref([
+  {
+    id: 1,
+    parent_element_id: 1,
+    title: 'Условия доступа к вкладкам',
+    img_code: '#img#id0234#',
+    text: 'Если пользователь является partner / creator / curator видит все Вкладки *|* Если пользователь числится в соучастниках по проекту видит: "План-график", "Заявки", "Соучастники", "ТМЦ" *|* Если пользователь является else - только "Соучастники"'
+  },
+  {
+    id: 2,
+    parent_element_id: 1,
+    title: 'План-график',
+    img_code: '',
+    text: null
+  },
+  {
+    id: 3,
+    parent_element_id: 1,
+    title: 'Соучастники',
+    img_code: '',
+    text: null
+  },
+  {
+    id: 4,
+    parent_element_id: 2,
+    title: 'Условия показа',
+    img_code: '#img#id0234#',
+    text: 'Пользователь является собственником предмета *|* Пользовтатель является ответственным по предмету (в случае, если флаг "Показывать всем в банде" отключен) *|* Предметы банды, если пользователь является участников банды *|* Предметы яльянса, если банда, соучастником которой является пользователь *|* Предметы других пользователей, если они состоят в банде или альянсе, как и сам пользователь'
+  },
+  {
+    id: 5,
+    parent_element_id: 2,
+    title: 'Скрытие показа предмета на складе',
+    img_code: '#img#id0234#',
+    text: 'Если пользователем является собственником предмета, он может скрыть предмет от показза другим участникам банды/альянса. *|* Скрытый предмет будет видеть только собствевннику и, если указан,  ответственному по данному предмету.'
+  }
+])
+
+// MAKE FUNCs
+// ***********************
+// make_string_split
+const make_string_split = (string:string, sign:string) => {
+  let result =  string.split('sign')
+  return [
+    result
+  ]
+}
+
 </script>
 
 <style scoped>
@@ -284,4 +372,12 @@ import { Container } from "@/shared/container";
   color: var(--bs-primary);
   cursor: pointer;
 }
+
+/* ARTICLE */
+.article_wrapper {
+  border-bottom: 1px solid var(--bs-border-color);
+  padding: 1rem 0;
+}
+
+/* ARTICLE ELEMENT */
 </style>
